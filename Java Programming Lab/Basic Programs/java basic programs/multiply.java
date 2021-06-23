bf4651a0a1eab9599ac8edcf00a8b6e2f2fa1875
19wh1a1291 <69686329+19wh1a1291@users.noreply.github.com>
2021-06-23 10:15:23 +0530

    Add files via upload

---
+++ Java Programming Lab/Basic Programs/java basic programs/multiply.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class multiply{
+public static void main(String[] args){
+int x,y,mul;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter 1st number:");
+x=sc.nextInt();
+System.out.println("enter 2nd number:");
+y=sc.nextInt();
+mul=x*y;
+System.out.println("multiplication:"+mul);
+}}
