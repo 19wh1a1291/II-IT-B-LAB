3db456fb2f3524090b8e8a2a52129e979562b2d5
19wh1a1291 <69686329+19wh1a1291@users.noreply.github.com>
2021-06-23 13:55:59 +0530

    Add files via upload

---
+++ Java Programming Lab/Basic Programs/Array programs/chartostring.java
@@ -0,0 +1,10 @@
+class chartostring
+{
+    public static void main(String[] args)
+    {
+        char[] charArray = { 'J', 'a', 'v', 'a' };
+ 
+        String str = new String(charArray);
+        System.out.println(str);
+    }
+}
