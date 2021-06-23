27391dbfd7033f7fca397687fc2abf46871f86c5
19wh1a1291 <69686329+19wh1a1291@users.noreply.github.com>
2021-06-23 10:14:35 +0530

    Add files via upload

---
+++ Java Programming Lab/Basic Programs/java basic programs/addcomplex.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class addcomplex{
+public static void main(String[] args){
+float x,y,sum;
+Scanner reader=new Scanner(System.in);
+System.out.println("enter 1st number:");
+x=reader.nextFloat();
+System.out.println("enter 2nd number:");
+y=reader.nextFloat();
+sum=x+y;
+System.out.println("sum is:"+sum);
+}}
+
