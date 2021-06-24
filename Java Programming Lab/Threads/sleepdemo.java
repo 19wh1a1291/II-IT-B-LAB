ca6f82fcb096ee4179c08af90f50b9ca4df1fe28
19wh1a1291 <69686329+19wh1a1291@users.noreply.github.com>
2021-06-24 14:29:56 +0530

    Add files via upload

---
+++ Java Programming Lab/Threads/sleepdemo.java
@@ -0,0 +1,30 @@
+class A extends Thread
+{
+int i =1;
+public void run()
+{
+while(i<=10)
+{
+
+System.out.println("i = "+i);
+try
+{
+Thread.sleep(1000);
+}
+catch(Exception e)
+{
+}
+i++;
+}
+}
+}
+
+
+
+class sleepdemo
+{
+public static void main(String args[])
+{
+A a = new A();
+a.start();
+}}
