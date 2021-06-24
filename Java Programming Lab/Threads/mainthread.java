582729d69032954a78d860bef8f9f97ca3475dd8
19wh1a1291 <69686329+19wh1a1291@users.noreply.github.com>
2021-06-24 14:30:36 +0530

    Add files via upload

---
+++ Java Programming Lab/Threads/mainthread.java
@@ -0,0 +1,11 @@
+public class mainthread
+{
+public static void main(String args[])
+{
+Thread t = Thread.currentThread();
+
+System.out.println(" Current Thread Name : " + t);
+t.setName("manisha");
+System.out.println("After Changing the Name : " +t);
+}
+}
